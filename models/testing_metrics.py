aea1c15cfb7ec404ad61ec53f41e814d58e42dda
wongsz33 <erinwongezsss1113@gmail.com>
2025-11-14 02:00:37 -0500

    Add files via upload

---
+++ models/testing_metrics.py
@@ -0,0 +1,25 @@
+from sklearn.metrics import (accuracy_score, precision_score, recall_score, f1_score, r2_score,
+                             mean_absolute_error, mean_squared_error, max_error, mean_absolute_percentage_error)
+
+def get_regression_metrics(model, X, y_true):
+    """
+    Get a dicionary with regression metrics,
+    including MAE, MSE, Max Error, MAPE, R2 Score
+    """
+    y_predicted = model.predict(X)
+
+    mae = mean_absolute_error(y_true, y_predicted)
+    mse = mean_squared_error(y_true, y_predicted)
+    maximum_error = max_error(y_true, y_predicted)
+    mape = mean_absolute_percentage_error(y_true, y_predicted)
+    r2 = r2_score(y_true, y_predicted)
+
+    metrics_dict = {
+        'mae': mae,
+        'mse': mse,
+        'max_error': maximum_error,
+        'mape': mape,
+        'r2': r2
+    }
+
+    return metrics_dict
